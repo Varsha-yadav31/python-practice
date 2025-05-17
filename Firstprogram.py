bf983755590f08350b3ba44e6284176672ffb913
Varsha <harshyadav@Harshs-MacBook-Air.local>
2025-05-17 19:24:38 +0530

    add odd even fn

--- Firstprogram.py
+++ Firstprogram.py
@@ -1,1 +1,18 @@
 print("hlo world")
+
+def print_name():
+    name = input("Please enter your name: ")
+    print(f"Hello, {name}!")
+
+# Call the function to run it
+print_name()
+
+
+def print_odd_even():
+    number = int(input("please enter your number: "))
+    if(number%2 == 0):
+        print("your number is even!")
+    else:
+        print("your number is odd!")
+
+print_odd_even()
