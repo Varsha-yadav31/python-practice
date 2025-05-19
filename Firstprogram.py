a2f4243d2b405149298f6ea2b795064dd207b1af
Varsha <harshyadav@Harshs-MacBook-Air.local>
2025-05-19 10:37:17 +0530

    print sum

--- Firstprogram.py
+++ Firstprogram.py
@@ -1,18 +1,16 @@
-print("hlo world")
+print("Hello World")
 
 def print_name():
     name = input("Please enter your name: ")
     print(f"Hello, {name}!")
 
-# Call the function to run it
-print_name()
-sdjsd
-
 def print_odd_even():
-    number = int(input("please enter your number: "))
-    if(number%2 == 0):
-        print("your number is even!")
+    number = int(input("Please enter your number: "))
+    if(number % 2 == 0):
+        print("Your number is even!")
     else:
-        print("your number is odd!")
+        print("Your number is odd!")
 
+# Call both functions
+print_name()
 print_odd_even()
