eee35ba2c6d726b6826826646a136fd7191b9cea
Varsha <harshyadav@Harshs-MacBook-Air.local>
2025-05-19 10:39:49 +0530

    sum of two numbers

---
+++ basic.py
@@ -0,0 +1,6 @@
+#indentation = means space at the begnning of the line 
+#casting = means represent the deta type of the variable
+a = int(input("Entre a : "))
+b = int(input("Entre b: "))
+sum = a + b
+print(sum)
