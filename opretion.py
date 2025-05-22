f2c7296d9115bd213b36a374d812a3ef911c42d2
Varsha <harshyadav@Harshs-MacBook-Air.local>
2025-05-22 14:28:08 +0530

    basic opretion of string

---
+++ opretion.py
@@ -0,0 +1,24 @@
+#WAC to conctinate two  string and print  there length 
+a = "varsha"
+b = "yadav"
+c = print(len(a))
+d = print(len(b))
+e = a + " " + b
+print(len(e),"\n",e)
+print("\n")
+
+# print index of string
+f = "hello varsha"
+print(f[4])
+print("\n")
+
+# slicing of string 
+#print (g[starting index : ending index])
+# it means to get the part of string 
+g = "helo world"
+print(g[0:3])
+
+# negative slicing 
+h = "helo world"
+print(h[-3:-1])
+
