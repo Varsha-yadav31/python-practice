5cc10f00f2c465e985d33ebf1719accd9d2a5387
Varsha <harshyadav@Harshs-MacBook-Air.local>
2025-05-22 12:28:45 +0530

    datatype

--- basic.py
+++ basic.py
@@ -1,6 +1,11 @@
 #indentation = means space at the begnning of the line 
 #casting = means represent the deta type of the variable
-a = int(input("Entre a : "))
+"""a = int(input("Entre a : "))
 b = int(input("Entre b: "))
 sum = a + b
-print(sum)
+print(sum)"""
+
+x = 5
+y = 10.0
+print(type(x))
+print(type(y))
--- type.py
+++ type.py
@@ -6,3 +6,10 @@ print(type(x))
 print(type(y))
 print(type(a))
 print(type(b))
+# python is a case sensitive language 
+# variable name should not start with number 
+#variable name sholud not contain special characters
+# variable name should not contain space 
+# variable name should not  be a keyword 
+
+
