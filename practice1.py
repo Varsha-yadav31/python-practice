74a6e59ad0a52d6b5fecb3761a683d876ea4dfcc
Varsha <harshyadav@Harshs-MacBook-Air.local>
2025-05-22 19:57:29 +0530

    practice1.py

---
+++ practice1.py
@@ -0,0 +1,9 @@
+#WAP to tacke input name and print its length ,type, and index 
+name = input("Entre name: ")
+print(len(name))
+print(type(name))
+print(name[0])
+
+#WAP to count th $ in the following program 
+r = "hello $ world $ name $ varsha$"
+print(r.count("$"))
