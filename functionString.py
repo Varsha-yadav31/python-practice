ba6c30e2038dcd97d1de2c9e3a9bcf5a413992cf
Varsha <harshyadav@Harshs-MacBook-Air.local>
2025-05-22 19:50:52 +0530

    FunctionString

---
+++ functionString.py
@@ -0,0 +1,6 @@
+str = " My name is varsha , im a student of computer science and engineering "
+print(str.count("a"))
+print(str.replace("a","A"))
+print(str.find("a"))
+print(str.index("of"))
+print(str.endswith("ing"))
