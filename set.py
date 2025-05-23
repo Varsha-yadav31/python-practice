ae01066c6d17d10199bec836443bd1df9ef0035d
Varsha <harshyadav@Harshs-MacBook-Air.local>
2025-05-23 14:41:07 +0530

    set

---
+++ set.py
@@ -0,0 +1,10 @@
+# also same as tuple,list 
+#it is unorderd , unchangeable , unindexed
+# but list is chngeable ,orderd , indexed
+
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+
+# Duplicate values will be ignored:
+thisset = {"apple", "banana", "cherry", "apple"}
+print(thisset)
