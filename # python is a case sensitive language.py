d1bb1cd8d95f7cac9561860422020d8aa1d94921
Varsha <harshyadav@Harshs-MacBook-Air.local>
2025-05-23 13:48:22 +0530

    list

---
+++ # python is a case sensitive language.py
@@ -0,0 +1,10 @@
+# python is a case sensitive language 
+# variable name should not start with number 
+#variable name sholud not contain special characters
+# variable name should not contain space 
+# variable name should not  be a keyword 
+x , y, z = "varsha ", "orange ","0.5 "
+print (x)
+print(y)
+print(z)
+# python allow many variable assingn in one line
---
+++ list.py
@@ -0,0 +1,9 @@
+#list are used o store multiple iteams" in a single variables
+myList = ["apple ", "banana" ,"cherry" , "orange" , "kiwi", "mango" , "apple" ]
+print(myList)
+#it allow dublicate values and it is changeable 
+#it can store any data type 
+print(myList(1:5))
+print(myList[2])
+print(myList[-3: -1])
+
