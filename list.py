ae025267363aef4b52f5546ad6b91ed1260ce65b
Varsha <harshyadav@Harshs-MacBook-Air.local>
2025-05-23 13:49:51 +0530

    list

--- list.py
+++ list.py
@@ -3,7 +3,7 @@ myList = ["apple ", "banana" ,"cherry" , "orange" , "kiwi", "mango" , "apple" ]
 print(myList)
 #it allow dublicate values and it is changeable 
 #it can store any data type 
-print(myList(1:5))
+print(myList[1:5])
 print(myList[2])
 print(myList[-3: -1])
 
