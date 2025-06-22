7902a8eca664dc5b19c02981e12956027c7d5809
Varsha-yadav31 <varshu31yadav@gmail.com>
2025-06-22 18:25:05 +0530

    Function.py

---
+++ function.py
@@ -0,0 +1,13 @@
+#calling function
+def my_function():
+  print("Hello from a function")
+
+my_function()
+
+#passing argument
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
